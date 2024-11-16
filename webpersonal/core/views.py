5453adc1d1d84453ef8607796519859437add233
mgonzalz <mgonzgarc@myuax.com>
2024-11-16 03:07:06 +0100

    templates creadas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -13,8 +13,8 @@ html_base = """
 def home(request):
     return render(request, "core/home.html")
 def about(request):
-    return HttpResponse(html_base + """<h2>Acerca de</h2><p>Me llamo Héctor y me encanta Django!</p>""")
+    return render(request, "core/about.html")
 def contact(request):
-    return HttpResponse(html_base + """<h2>Contacto</h2><p>Aquí os dejo mi email y mis redes sociales:</p><ul>
-    <li><a href="mailto:hola@hektorprofe.net">Email</a></li><li><a href="https://github.com/hcosta">Github</a></li>
-    <li><a href="https://youtube.com">Youtube</a></li></ul>""")
+    return render(request, "core/contact.html")
+def portfolio(request):
+    return render(request, "core/portfolio.html")
