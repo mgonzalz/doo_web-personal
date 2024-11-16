8eb841ccc0304a178c18684913ee63d9c97a3205
mgonzalz <mgonzgarc@myuax.com>
2024-11-16 02:47:25 +0100

    cambio de settings - sqlite3 por defecto - creación de superuser

--- webpersonal/webpersonal/settings.py
+++ webpersonal/webpersonal/settings.py
@@ -75,8 +75,8 @@ WSGI_APPLICATION = 'webpersonal.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.sqlite3', # ruta de la base de datos
+        'NAME': BASE_DIR / 'db.sqlite3', # nombre de la base de datos
     }
 }
 
@@ -103,7 +103,7 @@ AUTH_PASSWORD_VALIDATORS = [
 # Internationalization
 # https://docs.djangoproject.com/en/5.1/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'es'
 
 TIME_ZONE = 'UTC'
 
