17831910a1198fbed3c2c9c9907273e1817fac03
mgonzalz <mgonzgarc@myuax.com>
2024-11-16 03:00:11 +0100

    rutas completas

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,5 +1,20 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
+html_base = """
+<h1>Mi Web Personal</h1>
+<ul>
+    <li><a href="/">Portada</a></li>
+    <li><a href="/about/">Acerca de</a></li>
+    <li><a href="/contact/">Contacto</a></li>
+</ul>
+"""
+
 def home(request):
- return HttpResponse("<h1>Mi Web Personal</h1><h2>Portada</h2>")
+    return HttpResponse(html_base + """<h2>Bienvenidos</h2><p>Esto es la portada.</p>""")
+def about(request):
+    return HttpResponse(html_base + """<h2>Acerca de</h2><p>Me llamo Héctor y me encanta Django!</p>""")
+def contact(request):
+    return HttpResponse(html_base + """<h2>Contacto</h2><p>Aquí os dejo mi email y mis redes sociales:</p><ul>
+    <li><a href="mailto:hola@hektorprofe.net">Email</a></li><li><a href="https://github.com/hcosta">Github</a></li>
+    <li><a href="https://youtube.com">Youtube</a></li></ul>""")
--- webpersonal/webpersonal/urls.py
+++ webpersonal/webpersonal/urls.py
@@ -22,4 +22,6 @@ from core import views
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home, name="home"),
+    path('about/', views.about, name="about"),
+    path('contact/', views.contact, name="contact"),
 ]
