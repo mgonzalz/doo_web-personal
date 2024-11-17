451bfefec836cff81cdeb7b1bd276b9c045748e1
mgonzalz <mgonzgarc@myuax.com>
2024-11-17 01:20:41 +0100

    creación de los modelos - cada vez que se haga un cambio en este se debe de volver a crear la migración y migrar

--- webpersonal/portfolio/models.py
+++ webpersonal/portfolio/models.py
@@ -1,3 +1,13 @@
 from django.db import models
 
 # Create your models here.
+'''
+Representa una tabla dentro de la base de datos. Cada clase representa una tabla. Se deberán
+de crear las columnas de la tabla. Cada columna será un atributo de la clase.
+'''
+class Project(models.Model): # Hereda de models.Model. El nombre SIEMPRE en singular.
+    title = models.CharField(max_length=200)
+    description = models.TextField()
+    image = models.ImageField()
+    created = models.DateTimeField(auto_now_add=True)
+    updated = models.DateTimeField(auto_now=True)
