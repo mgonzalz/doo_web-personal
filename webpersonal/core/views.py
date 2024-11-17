81164f63e7792040587c0c60d8cd74d7ea75d994
mgonzalz <mgonzgarc@myuax.com>
2024-11-17 02:16:27 +0100

    limpieza

--- webpersonal/core/views.py
+++ webpersonal/core/views.py
@@ -1,14 +1,6 @@
 from django.shortcuts import render, HttpResponse
 
 # Create your views here.
-html_base = """
-<h1>Mi Web Personal</h1>
-<ul>
-    <li><a href="/">Portada</a></li>
-    <li><a href="/about/">Acerca de</a></li>
-    <li><a href="/contact/">Contacto</a></li>
-</ul>
-"""
 
 def home(request):
     return render(request, "core/home.html")
